33638600fee30931f73ea1610d801dc11d804b57
newtonkim <knewts98@gmail.com>
2021-09-20 15:36:55 +0300

    Add Jetstream and Livewire

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', [HomeController::class, 'index']);
-// Route::get('/home', 'HomeController@index');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
