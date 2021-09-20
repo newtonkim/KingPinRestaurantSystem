6e168c4df206d5b4ad3d2671cef8948acfa17bb8
newtonkim <knewts98@gmail.com>
2021-09-20 16:06:24 +0300

    Create admin and user as two separate entities

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
         return view('home');
+    }
+
+    public function redirects()
+    {
+        // $usertype = auth()->user()->usertype;
+        $usertype = Auth::user()->usertype;
+
+        if($usertype == '1'){
+            return view('admin.adminhome');
+        } else{
+            return view('/');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/redirects', [HomeController::class, 'redirects']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
