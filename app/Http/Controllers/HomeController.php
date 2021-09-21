b7f6a88a865185cc233ed143586002e198d832b2
newtonkim <knewts98@gmail.com>
2021-09-21 13:32:21 +0300

    Displaying data from the DB to UI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Food;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,18 +10,20 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $users = Food::all();
+        return view('home', compact('users'));
     }
 
     public function redirects()
     {
-        // $usertype = auth()->user()->usertype;
+        $users = Food::all();
+
         $usertype = Auth::user()->usertype;
 
         if($usertype == '1'){
             return view('admin.adminhome');
         } else{
-            return view('/home');
+            return view('/home', compact('users'));
         }
     }
 }
